d30206093daf838d18f97477453671d822034a66
ThiagoMorano <thiago.morano159@gmail.com>
2016-06-23 14:11:43 -0300

    Refactor snake.h and snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -1,20 +1,94 @@
 #include "snake.h"
 
 snake::snake() {
-	speed = 10.0f;
-	
-	texture.loadFromFile("snakes_head.png");
-	head.setTexture(texture);
+	x = 30;
+	y = 30;
+	body[0].x = x;
+	body[0].y = y;
+	directon = 0;
+	length = 1;
 }
 
-snake::~snake() {
+void snake::move() {
+	for(int i=length-1;i != 0;i--)	{
+		body[i].x = body[i-1].x;
+       	body[i].y = body[i-1].y;
+
+	}
+
+	switch(direction) {
+	case 1:
+		y--;
+		break;
+
+	case 2:
+		y++;
+		break;
+
+	case 3:
+		x++;
+		break;
+
+	case 4:
+		x--;
+		break;
+	}
+
+	body[0].x = x;
+	body[0].y = y;
 
 }
 
-void snake::Update() {
+void snake::setDirection(int dir) { direction = dir; }
 
+void snake::draw(sf::RenderWindow &window) {
+	for(int i=0;i<length;i++)
+		window.Draw(sf::Shape::Rectangle(body[i].x*10+1,body[i].y*10+1,body[i].x*10+9,body[i].y*10+9,sf::Color(0,255,0)));
 }
 
-void snake::Draw(sf::RenderWindow* window) {
-	window->draw(head);
+void snake::addBodyPiece() {
+	length++;
+	for(int i=length-1;i != 0;i--) {
+		body[i].x = body[i-1].x;
+		body[i].y = body[i-1].y;
+	}
+
+	body[0].x = x;
+	body[0].y = y;
 }
+
+bool snake::eatFood(food &Food) {
+	if(x == Food.xVal() && y == Food.yVal())
+	return true;
+	else
+	return false;
+}
+
+bool snake::wallHit() {
+	if(y == -1 || y == 20 || x == -1 || x == 20)
+		return true;
+	else
+		return false;
+}
+
+bool snake::bodyHit(){
+	for(int i=2;i<length;i++)
+	if(body[i].x == x && body[i].y == y)
+		return true;
+
+	return false;
+}
+
+bool snake::foodCollision(food &Food) {
+
+	/*
+		VERIFICAR A COR DA COMIDA, E SE NÃO ESTIVER NA LISTA, MORRE
+	*/
+	for(int i=0;i<length;i++)
+		if(body[i].x == Food.xVal() && body[i].y == Food.yVal())
+			return true;
+
+	return false;
+}
+
+~snake() {}
