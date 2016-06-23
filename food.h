1a2ad072c3300f04b2a0cb3f31cdfe07c8f3afc9
gucolombini <gustavogiordanocolombini@gmail.com>
2016-06-23 14:50:26 -0300

    fruit changes

--- food.h
+++ food.h
@@ -3,38 +3,4 @@
 #ifndef FOOD_H
 #define FOOD_H
 
-enum type FoodColour {
-	Blue, Yellow, Red, Green, 
-};
 
-class Food {
-	private:
-		sf::Texture texture;
-		FoodColour colour;
-	
-	public:
-		Food(FoodColour _colour) {
-			colour = _colour;
-			
-			switch(colour) {
-				case Blue:
-				//	texture = sf::LoadSprite("blue_square");
-					break;
-				case Yellow:
-				//	texture = sf::LoadSprite("yellow_square");
-					break;
-				case Red:
-				//	texture = sf::LoadSprite("red_square");
-					break;
-				case Green:
-				//	texture = sf::LoadSprite("green_square");
-					break;
-		}
-		~Food() {
-			//DELETE Texture
-		}
-
-	}
-};
-
-#endif
--- fruit.h
+++ fruit.h
@@ -1,38 +1,79 @@
-#pragma once
+#ifndef FRUIT_H
+#define FRUIT_H
+#include <SFML/Graphics.hpp>
 
-class cFruit
-{
+enum FoodColour { Blue, Yellow, Red, Green, Orange };
+
+/*class Food {
+private:
+	sf::Texture texture;
+	FoodColour colour;
+
+public:
+	Food (FoodColour _colour) {
+		colour = _colour;
+
+		switch (colour) {
+		case Blue:
+			//	texture = sf::LoadSprite("blue_square");
+			break;
+		case Yellow:
+			//	texture = sf::LoadSprite("yellow_square");
+			break;
+		case Red:
+			//	texture = sf::LoadSprite("red_square");
+			break;
+		case Green:
+			//	texture = sf::LoadSprite("green_square");
+			break;
+		}
+	}
+	~Food () {
+		//DELETE Texture
+	}
+};*/
+
+class cFruit {
 private:
 	int x;
 	int y;
 
 public:
-	cFruit()
-	{
+	cFruit() {
 		x = sf::Randomizer::Random(0, 19);
 		y = sf::Randomizer::Random(0, 19);
 	}
 
-	int xVal()
-	{
+	int xVal() {
 		return x;
 	}
 
-	int yVal()
-	{
+	int yVal() {
 		return y;
 	}
 
-	void draw(sf::RenderWindow & App)
-	{
-		App.Draw(sf::Shape::Circle(x * 10 + 5, y * 10 + 5, 5.0, sf::Color(255, 0, 0)));
+	void draw(sf::RenderWindow & App) {
+		FoodColour colour;
+		switch (colour) {
+		case Blue:
+			break;
+		case Red:
+			break;
+		case Orange:
+			break;
+		case Yellow:
+			break;
+		case Green:
+			App.Draw(sf::Shape::Circle(x * 10 + 5, y * 10 + 5, 5.0, sf::Color::Green));
+			break;
+		}
 	}
 
-	void generate()
-	{
+	void generate() {
 		x = sf::Randomizer::Random(0, 19);
 		y = sf::Randomizer::Random(0, 19);
 	}
 
 	~cFruit() {}
 };
+#endif
